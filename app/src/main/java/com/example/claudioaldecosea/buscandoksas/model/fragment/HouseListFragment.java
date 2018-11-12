3952fadd2c339712d560e0306c15cbf63a60d081
Claudio Aldecosea <clablues@gmail.com>
2018-11-11 21:04:03 -0300

    Ultima version del proyecto para curso android

--- app/src/main/java/com/example/claudioaldecosea/buscandoksas/model/fragment/HouseListFragment.java
+++ app/src/main/java/com/example/claudioaldecosea/buscandoksas/model/fragment/HouseListFragment.java
@@ -1,11 +1,9 @@
 package com.example.claudioaldecosea.buscandoksas.model.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.claudioaldecosea.buscandoksas.HouseDetailActivity;
 import com.example.claudioaldecosea.buscandoksas.R;
 import com.example.claudioaldecosea.buscandoksas.domain.House;
 import com.example.claudioaldecosea.buscandoksas.model.adapter.HouseListAdapter;
